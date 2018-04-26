a8876ff21eca8cc6851afac2432600646f43b930
Andrew <andrewjamen@gmail.com>
2018-04-25 21:02:43 -0500

    moving blocks

--- StackerAR/ViewController.swift
+++ StackerAR/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  StackerAR
+//  TestAR
 //
-//  Created by Andrew Amen on 4/25/18.
+//  Created by Andrew Amen on 4/20/18.
 //  Copyright © 2018 Andrew Amen. All rights reserved.
 //
 
@@ -11,70 +11,189 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    var allNodes: [SCNNode] = []
+    var nodeNum = 0;
+    var leftRight = "left"
+    var pos = 0.0;
+    var gameStarted = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
+        let scene = SCNScene(named: "art.scnassets/scene.scn")!
         // Set the scene to the view
         sceneView.scene = scene
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+        
+        
+        //place foundation block
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.2, height: 0.1, length: 0.2, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0,0,-0.5)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        node.name = String(nodeNum);
+        allNodes.append(node)
+        
+        nodeNum += 1;
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    func findNode(num: Int) -> SCNNode?{
+        for node in allNodes {
+            if (node.name == String(num)){
+                return node
+            }
+        }
+        return nil
+    }
+    
+    func setColor(_ node: SCNNode){
         
-        // Pause the view's session
-        sceneView.session.pause()
+        node.geometry?.firstMaterial?.diffuse.contents = getRandomColor()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    func getRandomColor() -> UIColor{
+        
+        let randomRed:CGFloat = CGFloat(drand48())
+        
+        let randomGreen:CGFloat = CGFloat(drand48())
+        
+        let randomBlue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    func moveBox(_ node: SCNNode){
+        
+        if (leftRight == "left"){
+            
+            node.runAction(SCNAction.moveBy(x: 2, y: 0, z: 0, duration: 2))
+            
+            leftRight = "right"
+        }
+        else{
+            
+            node.runAction(SCNAction.moveBy(x: -2, y: 0, z: 0, duration: 2))
+            
+            leftRight = "left"
+        }
+        
+        
+    }
+    
+    func setPostition(_ node: SCNNode){
+        
+        if (leftRight == "left"){
+            
+            node.position = SCNVector3(-1, pos, -0.5)
+            
+        }
+        else{
+            node.position = SCNVector3(1, pos, -0.5)
+        }
+        
+        
+    }
+    
+    
+    @objc func addBox() -> SCNNode{
+        
+        //initilize node
+        let newNode = SCNNode()
+        newNode.geometry = SCNBox(width: 0.2, height: 0.1, length: 0.2, chamferRadius: 0)
+        setColor(newNode)
+        newNode.name = String(nodeNum);
+        setPostition(newNode)
+        
+        
+        
+        
+        //add node to stack
+        self.sceneView.scene.rootNode.addChildNode(newNode)
+        
+        moveBox(newNode)
+        
+        
+        /*
+         Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(addBox), userInfo: nil, repeats: false)
+         
+         DispatchQueue.main.asyncAfter(deadline: .now() + 2) { // change 2 to desired number of seconds
+         newNode.removeFromParentNode()
+         }
+         
+         */
+        
+        
+        
+        
+        return newNode
+    }
+    
+    func fixBoxPos(_ position: SCNVector3){
+        
+        
+        //place foundation block
         let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+        node.geometry = SCNBox(width: 0.2, height: 0.1, length: 0.2, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = getRandomColor()
+        node.position = position
+        self.sceneView.scene.rootNode.addChildNode(node)
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if (gameStarted == false){
+            
+            pos += 0.1
+            
+            let newNode = addBox()
+            
+            allNodes.append(newNode)
+            nodeNum += 1;
+            
+            gameStarted = true
+        }
+        else{ //game has started
+            
+            let lastNode = findNode(num: nodeNum - 1)
+            let lastNodePos = lastNode!.position
+            
+            
+            fixBoxPos(lastNodePos)
+            
+            lastNode!.removeFromParentNode()
+            
+            pos += 0.1
+            
+            
+            let newNode = addBox()
+            
+            
+            
+            allNodes.append(newNode)
+            nodeNum += 1;
+            
+        }
+        
         
     }
 }
